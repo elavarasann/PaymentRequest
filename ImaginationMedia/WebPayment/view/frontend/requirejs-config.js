680e8d1858bcbaf03bdb7f0c755ce5c2e3188a79
Elavarasan Natarajan <elanatar@publicisgroupe.net>
2018-12-26 13:57:51 +0530

    files added

---
+++ ImaginationMedia/WebPayment/view/frontend/requirejs-config.js
@@ -0,0 +1,12 @@
+var config = {
+    "map": {
+        "*": {
+            "Magento_Checkout/js/sidebar": "ImaginationMedia_WebPayment/js/sidebar",
+            sidebar: "ImaginationMedia_WebPayment/js/sidebar",
+            "Magento_Checkout/js/proceed-to-checkout": "ImaginationMedia_WebPayment/js/proceed-to-checkout",
+            "Magento_Checkout/template/minicart/content.html":
+                "ImaginationMedia_WebPayment/template/minicart/content.html"
+            
+        }
+    }
+};
